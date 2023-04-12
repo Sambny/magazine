cd6fdb5e27346344be265e5bb3d5c6162b90cd00
PipouilleJohnson <killian.bouzera77150@gmail.com>
2023-04-12 12:16:08 +0200

    save 120423 12h16

--- ProjetGénéral/JS/accueil.js
+++ ProjetGénéral/JS/accueil.js
@@ -1,24 +1,33 @@
-// console.log("Bonjour !");
 
-// const boutonAccueil = document.querySelector('button').style.background = "pink";
-
-// console.log(boutonAccueil);
-
-//click event
-
-const boutonAccueil = document.querySelector(".button");
+const boutonUn = document.querySelector(".container-button-un");
+const boutonDeux = document.querySelector(".container-button-deux");
+const boutonTrois = document.querySelector(".container-button-trois");
+const boutonQuatre = document.querySelector(".container-button-quatre");
 
 const texteAccueil = document.querySelector(".container-main p");
 
-console.log(boutonAccueil);
-
-// boutonAccueil.style.borderRadius = "50px";
+console.log(boutonUn);
 
 texteAccueil.addEventListener("click", () =>{
     console.log("Le texte est selectionné");
     texteAccueil.style.opacity = 100;
 })
 
+// blague des boutons qui changent de placce
+boutonUn.addEventListener("click", ()=>{
+    boutonUn.style.visibility = "hidden";
+    boutonDeux.style.visibility = "visible";   
+})
+
+boutonDeux.addEventListener("click", ()=>{
+    boutonDeux.style.visibility = "hidden";
+    boutonTrois.style.visibility = "visible";  
+})
+
+boutonTrois.addEventListener("click", ()=>{
+    boutonTrois.style.visibility = "hidden";
+    boutonQuatre.style.visibility = "visible";  
+})
 
 
 
