ce0dd51d3fe9c61999d38376a317f06d6b64155d
PipouilleJohnson <killian.bouzera77150@gmail.com>
2023-05-09 14:36:15 +0200

    save 090523 14h36

--- ProjetGénéral/script/index.js
+++ ProjetGénéral/script/index.js
@@ -75,6 +75,87 @@ choixQuatre.addEventListener("click", ()=>{
     backgroundQuatre.style.visibility = "visible";   
 })
 
+//Choix des éléments
+const sansEl = document.querySelector("#sans-el");
+const elUn = document.querySelector("#el-un");
+const elDeux = document.querySelector("#el-deux");
+const elTrois = document.querySelector("#el-trois");
+const elQuatre = document.querySelector("#el-quatre");
+
+const assetUn = document.querySelector("#asset-un");
+const assetDeux = document.querySelector("#asset-deux");
+const assetTrois = document.querySelector("#asset-trois");
+const assetQuatre = document.querySelector("#asset-quatre");
+
+sansEl.addEventListener("click", ()=>{
+
+    elUn.style.border = "0px solid black";
+    elDeux.style.border = "0px solid black";
+    elTrois.style.border = "0px solid black";
+    elQuatre.style.border = "0px solid black";
+
+    assetUn.style.visibility = "hidden";
+    assetDeux.style.visibility = "hidden";
+    assetTrois.style.visibility = "hidden";
+    assetQuatre.style.visibility = "hidden";
+})
+
+elUn.addEventListener("click", ()=>{
+
+    elUn.style.border = "3px solid black";
+    elDeux.style.border = "0px solid black";
+    elTrois.style.border = "0px solid black";
+    elQuatre.style.border = "0px solid black";
+
+    assetUn.style.visibility = "visible";
+    assetDeux.style.visibility = "hidden";
+    assetTrois.style.visibility = "hidden";
+    assetQuatre.style.visibility = "hidden";
+})
+
+elDeux.addEventListener("click", ()=>{
+
+    elUn.style.border = "0px solid black";
+    elDeux.style.border = "3px solid black";
+    elTrois.style.border = "0px solid black";
+    elQuatre.style.border = "0px solid black";
+
+    assetUn.style.visibility = "hidden";
+    assetDeux.style.visibility = "visible";
+    assetTrois.style.visibility = "hidden";
+    assetQuatre.style.visibility = "hidden";
+})
+
+elTrois.addEventListener("click", ()=>{
+
+    elUn.style.border = "0px solid black";
+    elDeux.style.border = "0px solid black";
+    elTrois.style.border = "3px solid black";
+    elQuatre.style.border = "0px solid black";
+
+    assetUn.style.visibility = "hidden";
+    assetDeux.style.visibility = "hidden";
+    assetTrois.style.visibility = "visible";
+    assetQuatre.style.visibility = "hidden";
+})
+
+elQuatre.addEventListener("click", ()=>{
+
+    elUn.style.border = "0px solid black";
+    elDeux.style.border = "0px solid black";
+    elTrois.style.border = "0px solid black";
+    elQuatre.style.border = "3px solid black";
+
+    assetUn.style.visibility = "hidden";
+    assetDeux.style.visibility = "hidden";
+    assetTrois.style.visibility = "hidden";
+    assetQuatre.style.visibility = "visible";
+})
+
+
+
+
+
 
 // color picker
 
@@ -90,3 +171,17 @@ colorPicker.addEventListener("input", function() {
   // Application de la couleur sélectionnée à l'objet
   coloredObject.style.backgroundColor = selectedColor;
 });
+
+
+// Entrer le texte sur l'affiche 
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    var texteEntre = document.getElementById("entre-texte");
+    var renduTexte = document.getElementById("titre-rendu");
+
+    texteEntre.addEventListener('input', function(){
+        var texteTape = texteEntre.value;
+        renduTexte.innerHTML = texteTape;
+    })
+})
